78e5b42eadf879a719d940f13fbac570d7d8152c
Fernando Maymone <fmaymone@gmail.com>
2019-02-14 12:52:34 +0000

    MOD adding the email and rules on the return of the sign_in method

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,7 +4,8 @@ class AuthenticationController < ApplicationController
   def authenticate
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    user = User.where(email: auth_params[:email]).first
+    json_response(auth_token: auth_token, role: user.role, email: user.email)
   end
 
   private
